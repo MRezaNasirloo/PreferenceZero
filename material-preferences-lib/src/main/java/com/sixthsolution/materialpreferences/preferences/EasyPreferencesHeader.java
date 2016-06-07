e42abe1358d29287c3b481e94a71d08fcbb92796
mehdok <mehdok@gmail.com>
2016-06-07 18:14:18 +0430

    Update group header to be a container.

--- material-preferences-lib/src/main/java/com/sixthsolution/materialpreferences/preferences/EasyPreferencesHeader.java
+++ material-preferences-lib/src/main/java/com/sixthsolution/materialpreferences/preferences/EasyPreferencesHeader.java
@@ -2,7 +2,10 @@ package com.sixthsolution.materialpreferences.preferences;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.support.v7.widget.CardView;
 import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -12,10 +15,11 @@ import com.sixthsolution.materialpreferences.R;
 /**
  * Created by mehdok on 5/16/2016.
  */
-public class EasyPreferencesHeader extends LinearLayout {
+public class EasyPreferencesHeader extends CardView {
 
     private TextView tvTitle;
     private ImageView imgIcon;
+    private LinearLayout rootLayout;
 
     protected String tittle;
     protected int iconResId;
@@ -42,6 +46,7 @@ public class EasyPreferencesHeader extends LinearLayout {
     protected void init(Context context, AttributeSet attrs, int defStyleAttr) {
         inflate(context, getLayout(), this);
 
+        rootLayout = (LinearLayout) findViewById(R.id.category_root_layout);
         tvTitle = (TextView) findViewById(R.id.easy_group_title);
         imgIcon = (ImageView) findViewById(R.id.easy_group_icon);
 
@@ -61,5 +66,45 @@ public class EasyPreferencesHeader extends LinearLayout {
         }
 
         typedArray.recycle();
+    }
+
+    @Override
+    public void addView(View child) {
+        if (rootLayout != null)
+            rootLayout.addView(child);
+        else
+            super.addView(child);
+    }
+
+    @Override
+    public void addView(View child, int index) {
+        if (rootLayout != null)
+            rootLayout.addView(child, index);
+        else
+            super.addView(child, index);
+    }
+
+    @Override
+    public void addView(View child, int width, int height) {
+        if (rootLayout != null)
+            rootLayout.addView(child, width, height);
+        else
+            super.addView(child, width, height);
+    }
+
+    @Override
+    public void addView(View child, ViewGroup.LayoutParams params) {
+        if (rootLayout != null)
+            rootLayout.addView(child, params);
+        else
+            super.addView(child, params);
+    }
+
+    @Override
+    public void addView(View child, int index, ViewGroup.LayoutParams params) {
+        if (rootLayout != null)
+            rootLayout.addView(child, index, params);
+        else
+            super.addView(child, index, params);
     }
 }
